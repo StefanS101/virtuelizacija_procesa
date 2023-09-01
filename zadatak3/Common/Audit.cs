9f8c7c05aca3d2560b35b9acde40f517203e40d3
Stefan Stanimirovic <stanimirovicstefan522@gmail.com>
2023-09-01 16:43:06 +0200

    Load and Audit classses

---
+++ zadatak3/Common/Audit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Audit
+    {
+        private string id;
+        private DateTime timeStamp;
+        private MessageTypes messageType;
+        private string message;
+
+        public Audit(string id, DateTime timeStamp, MessageTypes messageType, string message)
+        {
+            this.Id = id;
+            this.TimeStamp = timeStamp;
+            this.MessageType = messageType;
+            this.Message = message;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public DateTime TimeStamp { get => timeStamp; set => timeStamp = value; }
+        public MessageTypes MessageType { get => messageType; set => messageType = value; }
+        public string Message { get => message; set => message = value; }
+    }
+}
---
+++ zadatak3/Common/Load.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Load
+    {
+        private string id;
+        private DateTime timeStamp;
+        private double forecastValue;
+        private double measuredValue;
+
+        public Load(string id, DateTime timeStamp, double forecastValue, double measuredValue)
+        {
+            this.Id = id;
+            this.TimeStamp = timeStamp;
+            this.ForecastValue = forecastValue;
+            this.MeasuredValue = measuredValue;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public DateTime TimeStamp { get => timeStamp; set => timeStamp = value; }
+        public double ForecastValue { get => forecastValue; set => forecastValue = value; }
+        public double MeasuredValue { get => measuredValue; set => measuredValue = value; }
+    }
+}
