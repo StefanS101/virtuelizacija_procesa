5897e72a3ad49a160289dde4505a9bd4b976eaa9
stefan <stanimirovicstefan522@gmail.com>
2023-09-03 01:10:33 +0200

    upis rezultata u csv datoteku
    kreirana csv logika:
    upis rezultata u csv datoteku sa imenom datuma u nazivu
    nedostaci: ne vraca dictiomary audit objekata koji odogvaraju load
    objektima
    dodati restartovanje InMemory baze na svakih 15 minuta

--- zadatak3/Client/Program.cs
+++ zadatak3/Client/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
+using System.Configuration;
 using System.ServiceModel;
 using System.Threading;
 
@@ -9,6 +9,34 @@ namespace Client
 {
     internal class Program
     {
+        static void process(IElectricityConsumption channel, DateTime time)
+        {
+            Tuple<List<Load>, Audit> results = channel.GetValues(time);
+            if (results.Item1 != null && results.Item1.Count > 0)
+            {
+                List<Load> loaded = results.Item1;
+                foreach (var result in loaded)
+                {
+                    Console.WriteLine(result.ToString() + "\n");
+                }
+
+                CSVManipulation csv = new CSVManipulation();
+
+
+                string direktorijumZaCsvDatoteka = ConfigurationManager.AppSettings["putanja"];
+                string imeDatoteke = "results_" + results.Item1[0].TimeStamp.ToString("yyyy_MM_dd") + ".csv";
+                string putanjaDatoteke = direktorijumZaCsvDatoteka + "\\" + imeDatoteke;
+
+
+
+                csv.MakeCSVFile(loaded, putanjaDatoteke);
+            }
+            else if (results.Item2 != null)
+            {
+                Console.WriteLine(results.Item2.ToString());
+            }
+        }
+
         static void Main(string[] args)
         {
 
@@ -35,40 +63,33 @@ namespace Client
             {
                 Console.WriteLine("1. current date");
                 Console.WriteLine("2. Custom date:");
-                Console.WriteLine("0. Exit");
+                Console.WriteLine("0. Exit application");
                 option = Int32.Parse(Console.ReadLine());
 
                 switch (option)
                 {
-                    case 1: break;
+                    case 1:
+                        process(channel, time);
+                        break;
                     case 2:
                         Console.WriteLine("unesite datum u formatu dd/mm/yyyy");
                         string s = Console.ReadLine();
                         string[] elementi = s.Split('/');
-                        Console.WriteLine(elementi);
-                        Console.WriteLine(elementi[0]);
-                        time = new DateTime(Int32.Parse(elementi[2]), Int32.Parse(elementi[1]), Int32.Parse(elementi[0]) );
+                        time = new DateTime(Int32.Parse(elementi[2]), Int32.Parse(elementi[1]), Int32.Parse(elementi[0])); process(channel, time);
                         break;
                     case 3:
-                        throw new Exception();
-                        
+                        break;
+
+
+
                 }
 
 
-                Tuple<List<Load>, Audit> results = channel.GetValues(time);
-                if (results.Item1 != null)
-                {
-                    List<Load> loaded = results.Item1;
-                    foreach (var result in loaded)
-                    {
-                        Console.WriteLine(result.ToString() + "\n");
-                    }
-                }
+
 
 
             } while (option != 0);
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
+            
         }
     }
 }
